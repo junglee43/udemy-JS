bb0373706822cdbf6307c546db1711e1f65d5cc0
Jung Lee <jung@jungs-macbook.local>
2018-11-28 21:44:02 -0500

    Fixed variable undefined bug for the addListItem method

--- starter/app.js
+++ starter/app.js
@@ -1,5 +1,4 @@
 var budgetController = (function() {
-
     var Expense = function(id, description, value) {
         this.id = id;
         this.description = description;
@@ -38,8 +37,9 @@ var budgetController = (function() {
             if(type === 'expense') {
                 newItem = new Expense(ID, des, val);
             } else if(type === 'income') {
-                newItem = new Expense(ID, des, val);
+                newItem = new Income(ID, des, val);
             }
+
             // Push into data structure
             data.allItems[type].push(newItem);
 
@@ -61,17 +61,42 @@ var UIcontroller = (function() {
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        inputBtn: '.add__btn'
+        inputBtn: '.add__btn',
+        incomeContainer: '.income__list',
+        expensesContainer: '.expenses__list'
     };
 
     return {
         getInput: function() {
             return {
                 type: document.querySelector(DOMstrings.inputType).value,
-                description:  document.querySelector(DOMstrings.inputDescription).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
                 value: document.querySelector(DOMstrings.inputValue).value
             };
         },
+
+        addListItem: function(obj, type) {
+            var html, newHtml, element;
+
+            // Create HTML string with placeholder text
+            if(type === 'income') {
+                element = DOMstrings.incomeContainer;
+                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            } else if(type === 'expense') {
+                element = DOMstrings.expensesContainer;
+                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            }
+            // Testing newItem undefined
+            console.log(html, newHtml);
+            // Replace the placeholder text with actual data
+            newHtml = html.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%description%', obj.description);
+            newHtml = newHtml.replace('%value%', obj.value);
+
+            // Insert the HTML into the DOM
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+        },
+
         // Add a function to expose the DOMstring object to public
         // to enable the Global app controller to use it
         getDOMstrings: function() {
@@ -102,8 +127,12 @@ var appController = (function(budgetCtrl, UICtrl) {
 
         // 2. Add the item to the budgetController
         newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+        // Testing newItem undefined error
+        console.log(newItem);
 
         // 3. Update the UI with item
+        UICtrl.addListItem(newItem, input.type);
+
         // 4. Calculate the budget
         // 5. Display new budget on UI
         console.log('It works.');
