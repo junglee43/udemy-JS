3c7cfb48180c9f29b9656250471c5429e37df8c4
Jung Lee <jung@jungs-macbook.local>
2018-11-29 11:14:11 -0500

    lecture86 bug in budget calculation

--- starter/app.js
+++ starter/app.js
@@ -11,6 +11,15 @@ var budgetController = (function() {
         this.value = value;
     };
 
+    var calculateTotal = function(type) {
+        var sum = 0;
+        data.allItems[type].forEach(function(current) {
+            sum = current.value;
+        });
+
+        data.totals[type] = sum;
+    };
+
     var data = {
         allItems: {
             expense: [],
@@ -19,7 +28,9 @@ var budgetController = (function() {
         totals: {
             expense: 0,
             income: 0
-        }
+        },
+        budget: 0,
+        percentage: -1
     };
 
     return {
@@ -47,6 +58,28 @@ var budgetController = (function() {
             return newItem;
         },
 
+        calculateBudget: function() {
+            // Calculate total income and expenses
+            calculateTotal('expense');
+            calculateTotal('income');
+
+            // Calculate the budget: income - expenses
+            data.budget = data.totals.income - data.totals.expense;
+
+            // Calculate the percentage of income that we spent
+            data.percentage = Math.round((data.totals.expense / data.totals.income) * 100);
+        },
+
+        // New method to return budget calculations so that we can pass that to the globa and UI controllers
+        getBudget: function() {
+            return {
+                budget: data.budget,
+                totalIncome: data.totals.income,
+                totalExpense: data.totals.expense,
+                percentage: data.percentage
+            };
+        },
+
         testing: function() {
             console.log(data);
         }
@@ -71,7 +104,7 @@ var UIcontroller = (function() {
             return {
                 type: document.querySelector(DOMstrings.inputType).value,
                 description: document.querySelector(DOMstrings.inputDescription).value,
-                value: document.querySelector(DOMstrings.inputValue).value
+                value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
             };
         },
 
@@ -86,8 +119,7 @@ var UIcontroller = (function() {
                 element = DOMstrings.expensesContainer;
                 html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
             }
-            // Testing newItem undefined
-            console.log(html, newHtml);
+
             // Replace the placeholder text with actual data
             newHtml = html.replace('%id%', obj.id);
             newHtml = newHtml.replace('%description%', obj.description);
@@ -101,6 +133,21 @@ var UIcontroller = (function() {
         // to enable the Global app controller to use it
         getDOMstrings: function() {
             return DOMstrings;
+        },
+
+        // Clears all fields after ENTER, looping over each element of the array
+        clearFields: function() {
+            var fields, fieldsArray;
+            fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+
+            // A trick to coerce a list which is what is returned from queryselectorall into an array to be able to use array methods on the object
+            fieldsArray = Array.prototype.slice.call(fields);
+            fieldsArray.forEach(function(current, index, array) {
+                current.value = '';
+            });
+
+            // Sets the browser focus back to the Description field for easy entry
+            fieldsArray[0].focus();
         }
     };
 })();
@@ -119,23 +166,40 @@ var appController = (function(budgetCtrl, UICtrl) {
         });
     };
 
+    var updateBudget = function() {
+        // 1. Calculate the buget
+        budgetCtrl.calculateBudget();
+
+        // 2. Return the budget
+        var budget = budgetCtrl.getBudget();
+
+        // 3. Display the budget on the UI
+        console.log(budget);
+    };
+
     var ctrlAddItem = function() {
         // 1. Get the data from input field
         var input, newItem;
         input = UICtrl.getInput();
-        console.log(input);
+        // console.log(input);
 
-        // 2. Add the item to the budgetController
-        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
-        // Testing newItem undefined error
-        console.log(newItem);
+        // To validate the inputs before creating new data
+        if(input.description !== '' && !isNaN(input.value) && input.value > 0) {
 
-        // 3. Update the UI with item
-        UICtrl.addListItem(newItem, input.type);
+            // 2. Add the item to the budgetController
+            newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+            // Testing newItem undefined error
+            console.log(newItem);
 
-        // 4. Calculate the budget
-        // 5. Display new budget on UI
-        console.log('It works.');
+            // 3. Update the UI with item
+            UICtrl.addListItem(newItem, input.type);
+
+            // 4. Clear input fields in the UI
+            UICtrl.clearFields();
+
+            // 5. Caluclate and update budget
+            updateBudget();
+        }
     };
 
     return {
