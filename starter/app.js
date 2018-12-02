5ab4e82b60a06e0edb2d6a0b234fd33f7ca71567
Jung Lee <junglee43@gmail.com>
2018-12-01 19:27:09 -0500

    Fixed bug in delete item method.

--- starter/app.js
+++ starter/app.js
@@ -254,7 +254,7 @@ var appController = (function(budgetCtrl, UICtrl) {
         if(itemID) {
             splitID = itemID.split('-');
             type = splitID[0];
-            ID = splitID[1];
+            ID = parseInt(splitID[1]);
 
             // 1. Delete the item from the data structure
             budgetCtrl.deleteItem(type, ID);
